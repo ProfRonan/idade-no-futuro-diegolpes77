9907f1e028ec55ad308566bdfa6de69b922cff48
Diego Lopes <dieolpes77@gmail.com>
2023-05-20 22:35:46 -0300

    refiz a atividade de idade no futuro

--- main.py
+++ main.py
@@ -1,36 +1,9 @@
 ano_atual = int(input("Digite o ano atual: "))
 idade_atual = int(input("Digite a sua idade atual: "))
 ano_desejado = int(input("Digite o ano desejado:"))
-nome = input("Digite o seu nome: fulano")
+nome = input("Digite o seu nome:")
 idade_desejada = idade_atual + (ano_desejado - ano_atual)
 
 print(nome + ", no ano de " + str(ano_desejado) + " você terá " + str(idade_desejada) + " anos.")
 
-
-ano_atual = int(input("Digite o ano atual:"))
-idade_atual = int(input("Digite a sua idade atual: "))
-ano_desejado = int(input("Digite o ano desejado:"))
-nome = input("Digite o seu nome: Beltrano")
-idade_desejada = idade_atual + (ano_desejado - ano_atual)
-
-
-print(nome + ", no ano de " + str(ano_desejado) + " você terá " + str(idade_desejada) + " anos.")
-
-
-ano_atual = int(input("Digite o ano atual: "))
-idade_atual = int(input("Digite a sua idade atual: "))
-ano_desejado = int(input("Digite o ano desejado: "))
-nome = input("Digite o seu nome: Ciclano")
-idade_desejada = idade_atual + (ano_desejado - ano_atual)
-
-print(nome + ", no ano de " + str(ano_desejado) + " você terá " + str(idade_desejada) + " anos.")
-
-
-ano_atual = int(input("Digite o ano atual: "))
-idade_atual = int(input("Digite a sua idade atual: "))
-ano_desejado = int(input("Digite o ano desejado: "))
-nome = input("Digite o seu nome: Alice")
-idade_desejada = idade_atual + (ano_desejado - ano_atual)
-
-print(nome + ", no ano de " + str(ano_desejado) + " você terá " + str(idade_desejada) + " anos.")
 
